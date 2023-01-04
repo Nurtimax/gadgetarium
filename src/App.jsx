06470b01a05d44416e29c2a80b4857a61e9576e4
Alisher <zumanovaliser814@gmail.com>
2023-01-04 20:34:32 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-import { Button } from "@mui/material";
-import { useState } from "react";
 import "./App.css";
-import Modal from "./components/UI/Modal";
 
 const App = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-  return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
-      <Modal handleClose={handleClose} open={open}>
-        Alisher
-      </Modal>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
