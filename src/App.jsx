c7aaea59d9b461d676772c6181b022885b1dbffd
Alisher <zumanovaliser814@gmail.com>
2023-01-04 19:39:51 +0600

    create reusable snackbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,7 @@
 import React from "react";
-import PopUp from "./components/UI/PopUp";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import { useState } from "react";
-import { styled } from "@mui/material";
 
 const App = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  const action = (
-    <>
-      <FirstTitle>Перейти в избранное</FirstTitle>
-      <IconButton
-        size="small"
-        aria-label="close"
-        color="inherit"
-        onClick={handleClose}
-      >
-        <CloseIcon fontSize="small" />
-      </IconButton>
-    </>
-  );
-  return (
-    <>
-      <Button onClick={handleClick}>Show Snackbar</Button>
-      <PopUp
-        handleClose={handleClose}
-        action={action}
-        open={open}
-        duration={6000}
-        title={"Добавить в избранное"}
-      />
-    </>
-  );
+  return <div></div>;
 };
 
 export default App;
-
-const FirstTitle = styled("div")(() => ({
-  color: " rgba(60, 222, 20, 1)",
-}));
