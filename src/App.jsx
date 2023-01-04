58bc76d7fea79d173cdfe232a168186decbb95cd
Alisher <zumanovaliser814@gmail.com>
2023-01-04 20:31:12 +0600

    fixed design modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { Button } from "@mui/material";
+import { useState } from "react";
 import "./App.css";
+import Modal from "./components/UI/Modal";
 
 const App = () => {
-  return <div></div>;
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Modal handleClose={handleClose} open={open}>
+        Alisher
+      </Modal>
+    </div>
+  );
 };
 
 export default App;
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,11 +1,27 @@
-import { Dialog, DialogContent } from "@mui/material";
+import { Dialog, DialogContent, styled } from "@mui/material";
+import Icon from "../../assets/icons/closeIcon.png";
 
 const Modal = ({ children, open, handleClose }) => {
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogContent>{children}</DialogContent>
+      <ContainerIcon>
+        <img src={Icon} alt="closeIcon" />
+      </ContainerIcon>
+      <ContainerModal>{children}</ContainerModal>
     </Dialog>
   );
 };
 
 export default Modal;
+
+const ContainerModal = styled(DialogContent)(() => ({
+  background: "#FFFFFF",
+  boxShadow: "0px 10px 30px rgba(133, 143, 164, 0.1)",
+  borderRadius: "4px",
+}));
+
+const ContainerIcon = styled("div")(() => ({
+  display: "flex",
+  justifyContent: "flex-end",
+  padding: "15px 15px 0 0",
+}));
