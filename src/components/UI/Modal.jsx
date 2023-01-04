a16671c65e8954faa607f434ffa24e6882f70dee
Alisher <zumanovaliser814@gmail.com>
2023-01-04 19:19:19 +0600

    fixed all

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -2,11 +2,9 @@ import { Dialog, DialogContent } from "@mui/material";
 
 const Modal = ({ children, open, handleClose }) => {
   return (
-    <>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogContent>{children}</DialogContent>
-      </Dialog>
-    </>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogContent>{children}</DialogContent>
+    </Dialog>
   );
 };
 
