d711ca041589c45e73b41a6a7c61e338d045b344
Alisher <zumanovaliser814@gmail.com>
2023-01-05 12:45:48 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+// import React, { useState } from "react";
+// import PopUp from "./components/UI/PopUp";
+
+// const App = () => {
+//   const [open, setOpen] = useState(false);
+
+//   const handleClick = () => {
+//     setOpen(true);
+//   };
+
+//   const handleClose = () => {
+//     setOpen(false);
+//   };
+
+//   const transitionHandler = () => {
+//     //
+//   };
+
+//   return (
+//     <>
+//       <button onClick={handleClick}>Open</button>
+//       {open && (
+//         <PopUp
+//           handleClose={handleClose}
+//           transitionHandler={transitionHandler}
+//           added={"Добавлен в избранное!"}
+//           transition={"Перейти в избранное"}
+//         />
+//       )}
+//     </>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return <div>App</div>;
 };
 
 export default App;
--- src/components/UI/PopUp.jsx
+++ src/components/UI/PopUp.jsx
@@ -1,15 +1,81 @@
-import { Snackbar } from "@mui/material";
+import { styled } from "@mui/material";
+import { useEffect } from "react";
+import Icon from "../../assets/icons/iconClose.png";
+const PopUp = ({ handleClose, transitionHandler, added, transition }) => {
+  useEffect(() => {
+    const time = setTimeout(() => {
+      handleClose();
+    }, 4000);
+    return () => {
+      clearTimeout(time);
+    };
+  }, []);
 
-const PopUp = ({ handleClose, action, open, duration, title }) => {
   return (
-    <Snackbar
-      open={open}
-      autoHideDuration={duration}
-      onClose={handleClose}
-      message={title}
-      action={action}
-    />
+    <>
+      <Container>
+        <Added>{added}</Added>
+        <Transition onClick={transitionHandler}>{transition}</Transition>
+        <IconClose src={Icon} alt="Icon" onClick={handleClose} />
+      </Container>
+    </>
   );
 };
 
 export default PopUp;
+
+//Style
+
+const Container = styled("div")(() => ({
+  width: "470px",
+  height: "45px",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "20px 18px",
+  gap: "36px",
+  background: "#202027",
+  borderRadius: "4px",
+  position: "absolute",
+  animationName: "popUp",
+  animationDuration: "infinity",
+
+  "@keyframes popUp": {
+    "0%": {
+      top: 0,
+      right: 0,
+    },
+    "100%": {
+      right: "30%",
+    },
+  },
+}));
+
+const Added = styled("div")(() => ({
+  fontFamily: "Inter",
+  fontStyle: "normal",
+  fontSize: "18px",
+  lineHeight: "140%",
+  textAlign: "center",
+  color: "white",
+  flex: "none",
+  order: "0",
+  flexGrow: "0",
+}));
+
+const Transition = styled("div")(() => ({
+  fontFamily: "Inter",
+  fontStyle: "normal",
+  fontWeight: "700",
+  fontSize: "18px",
+  lineHeight: "130%",
+  color: "#3cde14",
+  flex: "none",
+  flexGrow: "0",
+}));
+
+const IconClose = styled("img")(() => ({
+  width: "12px",
+  height: "12px",
+}));
