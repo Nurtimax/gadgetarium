e6147448ba5b2bb8ff1667695b7558662eacb7f4
Alisher <zumanovaliser814@gmail.com>
2023-01-05 17:19:30 +0600

    fixed drop down

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import { useState } from "react";
-import DropDown from "./components/UI/DropDown";
-
 const App = () => {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <>
-      <button type="submit" onClick={() => setOpen(!open)}>
-        Open
-      </button>
-      {open && (
-        <DropDown propsWidth="300px" propsHeight="200px">
-          Alisher
-        </DropDown>
-      )}
-    </>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/components/UI/DropDown.jsx
+++ src/components/UI/DropDown.jsx
@@ -1,19 +1,33 @@
 import { styled } from "@mui/material";
 
-const DropDown = ({ children, propsWidth, propsHeight }) => {
+const DropDown = ({ children, propsWidth, propsHeight, open }) => {
   return (
-    <Container propsWidth={propsWidth} propsHeight={propsHeight}>
-      {children}
-    </Container>
+    <>
+      {open && (
+        <Container propsWidth={propsWidth} propsHeight={propsHeight}>
+          {children}
+        </Container>
+      )}
+    </>
   );
 };
 
 export default DropDown;
 
 const Container = styled("div")((props) => ({
-  width: props.propsWidth || "20px",
-  height: props.propsHeight || "10px",
+  width: props.propsWidth || "200px",
+  height: props.propsHeight || "200px",
   background: "#FFFFFF",
   boxShadow: "0px 4px 16px rgba(0, 0, 0, 0.1)",
   borderRadius: "4px",
+  animation: "modal 0.5s ease-out",
+
+  "@keyframes modal": {
+    "0%": {
+      transform: "scale(0.5)",
+    },
+    "100%": {
+      transform: "scale(1)",
+    },
+  },
 }));
