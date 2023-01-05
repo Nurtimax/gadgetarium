af8d135b384f60cab73f34c13ea47da9f2ab3a14
Alisher <zumanovaliser814@gmail.com>
2023-01-05 21:11:15 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import { useState } from "react";
-import Modal from "./components/UI/Modal";
-
 const App = () => {
-  const [open, setOpen] = useState(false);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const state = true;
-
-  return (
-    <div>
-      <button onClick={() => setOpen(true)}>open</button>
-      <Modal handleClose={handleClose} open={open} state={state}>
-        Modalfdsaaaadsfsaffsdafdsfsdafsfsdafdsafassd
-        fsdafdsafdsafdsfsdafasdfdsafdsfasfdsfasfdsfsd
-        fdasfsafdsafsdfsdafasdafsdafdasfsdafafdasfdsfsd
-      </Modal>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
