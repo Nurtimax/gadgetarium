9c018fde9ba679e1e018886f003215216f15239f
kaamilov <kaamilovmalik@gmail.com>
2023-01-06 01:05:18 +0300

    error

---
+++ src/components/UI/Themes.jsx
@@ -0,0 +1,42 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import React from "react";
+
+const Themes = ({ children }) => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        dark: "#292929",
+        main: "#1A1A25",
+        light: "#384255",
+        contrastText: "#fff",
+      },
+      secondary: {
+        light: "#2C68F5",
+        main: "#CB11AB",
+        dark: "#F99808",
+        contrastText: "#fff",
+      },
+      success: {
+        main: "#2FC504",
+        light: "#3CDE14",
+        dark: "#08A592",
+      },
+      error: {
+        main: "#F10000",
+      },
+      background: {
+        default: "#F4F4F4",
+      },
+      grey: {
+        200: "#E0E2E7",
+        400: "#E8E8E8",
+        600: "#CDCDCD",
+        800: "#91969E",
+        900: "#909CB5",
+      },
+    },
+  });
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default Themes;
---
+++ src/components/UI/input/Input.jsx
@@ -0,0 +1,36 @@
+import { InputBase, styled } from "@mui/material";
+import { forwardRef } from "react";
+
+const Input = forwardRef(({ value, onChange, error, ...props }, ref) => {
+  return (
+    <InputStyled
+      {...props}
+      value={value}
+      onChange={onChange}
+      error={error}
+      classes={{ root: "input", focused: "focused", error: "error" }}
+      ref={ref}
+    />
+  );
+});
+Input.displayName = Input;
+export default Input;
+
+const InputStyled = styled(InputBase)(({ theme }) => ({
+  "&.input": {
+    border: `0.1px solid ${theme.palette.grey[900]}`,
+    background: `${theme.palette.background.default}`,
+    borderRadius: "5px",
+    padding: "0 10px",
+  },
+  "&.input.focused": {
+    border: `0.1px solid ${theme.palette.secondary.main}`,
+    background: `${theme.palette.background.default}`,
+    borderRadius: "5px",
+    color: `${theme.palette.primary.dark}`,
+  },
+  "&.input.error": {
+    border: `1px solid ${theme.palette.error.main}`,
+    borderRadius: "5px",
+  },
+}));
