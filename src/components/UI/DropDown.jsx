177ece25da038998065123037412c4d1aac19a21
Alisher <zumanovaliser814@gmail.com>
2023-01-06 11:43:50 +0600

    save

--- src/components/UI/DropDown.jsx
+++ src/components/UI/DropDown.jsx
@@ -1,33 +1,7 @@
-import { styled } from "@mui/material";
+import React from "react";
 
-const DropDown = ({ children, propsWidth, propsHeight, open }) => {
-  return (
-    <>
-      {open && (
-        <Container propsWidth={propsWidth} propsHeight={propsHeight}>
-          {children}
-        </Container>
-      )}
-    </>
-  );
+const DropDown = () => {
+  return <div>DropDown</div>;
 };
 
 export default DropDown;
-
-const Container = styled("div")((props) => ({
-  width: props.propsWidth || "200px",
-  height: props.propsHeight || "200px",
-  background: "#FFFFFF",
-  boxShadow: "0px 4px 16px rgba(0, 0, 0, 0.1)",
-  borderRadius: "4px",
-  animation: "modal 0.5s ease-out",
-
-  "@keyframes modal": {
-    "0%": {
-      transform: "scale(0.5)",
-    },
-    "100%": {
-      transform: "scale(1)",
-    },
-  },
-}));
