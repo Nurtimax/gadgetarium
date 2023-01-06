6df1b00afc339947edf8268e901101316d24723f
Alisher <zumanovaliser814@gmail.com>
2023-01-06 11:56:53 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import DropDown from "./components/UI/DropDown";
+
 const App = () => {
-  return <div></div>;
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <button onClick={() => setOpen(true)}>open</button>
+      <DropDown
+        width="300px"
+        height="300px"
+        open={open}
+        handleClose={handleClose}
+      >
+        Alisher
+      </DropDown>
+    </div>
+  );
 };
 
 export default App;
--- src/components/UI/DropDown.jsx
+++ src/components/UI/DropDown.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { Menu, styled } from "@mui/material";
 
-const DropDown = () => {
-  return <div>DropDown</div>;
+const DropDown = ({ children, width, height, open, handleClose }) => {
+  return (
+    open && (
+      <Container
+        width={width}
+        height={height}
+        open={open}
+        onClose={handleClose}
+        classes={{ root: "dropDown" }}
+      >
+        {children}
+      </Container>
+    )
+  );
 };
 
 export default DropDown;
+
+const Container = styled(Menu)((props) => ({
+  dropDown: {
+    width: props.width || "200px",
+    height: props.height || "200px",
+    background: "#FFFFFF",
+    boxShadow: "0px 4px 16px rgba(0, 0, 0, 0.1)",
+    borderRadius: "4px",
+  },
+  //   animation: "modal 0.5s ease-out",
+
+  //   "@keyframes modal": {
+  //     "0%": {
+  //       transform: "scale(0.5)",
+  //     },
+  //     "100%": {
+  //       transform: "scale(1)",
+  //     },
+  //   },
+}));
