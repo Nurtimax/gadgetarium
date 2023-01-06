a645a1c7913dbf7f4300f42e8d9925bfefb45a28
kaamilov <kaamilovmalik@gmail.com>
2023-01-06 12:19:44 +0300

    footer finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import React from "react";
-import Footer from "./layout/Footer/Footer";
 const App = () => {
-  return (
-    <div>
-      <Footer>Alisher</Footer>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
