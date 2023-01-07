5c2f8ae7c44f761e9ee18dd8850391395ee031a2
kaamilov <kaamilovmalik@gmail.com>
2023-01-07 19:27:10 +0300

    icon button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import IconButton from "./components/UI/IconButton";
 const App = () => {
-  return (
-    <div>
-      <IconButton fondsize="40px">в карзину</IconButton>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
