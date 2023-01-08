621c2ecae54362a982e6d74af16e5815c795a1f8
Alisher <zumanovaliser814@gmail.com>
2023-01-08 23:51:38 +0600

    fixed

--- src/components/UI/DropDown.jsx
+++ src/components/UI/DropDown.jsx
@@ -5,15 +5,10 @@ const DropDown = ({ children, open, handleClose, vertical, horizontal }) => {
     <Container
       open={open}
       onClose={handleClose}
-      classes={{ root: "dropDown" }}
       PopoverClasses={{ paper: "paper" }}
       anchorOrigin={{
-        vertical: { vertical },
-        horizontal: { horizontal },
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "left",
+        vertical,
+        horizontal,
       }}
     >
       {children}
@@ -29,7 +24,7 @@ const Container = styled(Menu)(() => ({
     boxShadow: "0px 4px 16px rgba(0, 0, 0, 0.1)",
     borderRadius: "4px",
     position: "absolute",
-    padding: "5px",
+    padding: "50px",
     top: 0,
     left: 0,
   },
