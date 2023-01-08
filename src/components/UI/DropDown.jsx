a79997fb06697011f6c049572e9089b772258b6d
Alisher <zumanovaliser814@gmail.com>
2023-01-08 20:17:18 +0600

    fixed open and padding

--- src/components/UI/DropDown.jsx
+++ src/components/UI/DropDown.jsx
@@ -2,24 +2,22 @@ import { Menu, styled } from "@mui/material";
 
 const DropDown = ({ children, open, handleClose, vertical, horizontal }) => {
   return (
-    open && (
-      <Container
-        open={open}
-        onClose={handleClose}
-        classes={{ root: "dropDown" }}
-        PopoverClasses={{ paper: "paper" }}
-        anchorOrigin={{
-          vertical: { vertical },
-          horizontal: { horizontal },
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-      >
-        {children}
-      </Container>
-    )
+    <Container
+      open={open}
+      onClose={handleClose}
+      classes={{ root: "dropDown" }}
+      PopoverClasses={{ paper: "paper" }}
+      anchorOrigin={{
+        vertical: { vertical },
+        horizontal: { horizontal },
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+    >
+      {children}
+    </Container>
   );
 };
 
@@ -27,12 +25,11 @@ export default DropDown;
 
 const Container = styled(Menu)(() => ({
   "& .paper": {
-    width: "200px",
-    height: "200px",
     background: "#fffff",
     boxShadow: "0px 4px 16px rgba(0, 0, 0, 0.1)",
     borderRadius: "4px",
     position: "absolute",
+    padding: "5px",
     top: 0,
     left: 0,
   },
