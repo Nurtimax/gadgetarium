5e83f69af56938b60efaf68dc0b2ab414b24a64d
Alisher <zumanovaliser814@gmail.com>
2023-01-09 11:52:55 +0600

    fixed anchorEl

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,5 @@
-import { Button, MenuItem } from "@mui/material";
-import { useState } from "react";
-import DropDown from "./components/UI/DropDown";
-
 const App = () => {
-  const [open, setOpen] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const a = Boolean(anchorEl);
-  const handleClose = () => {
-    setOpen(false);
-    setAnchorEl(null);
-  };
-  const handleClick = (e) => {
-    setOpen(true);
-    setAnchorEl(e.currentTarget);
-  };
-  return (
-    <div>
-      <Button
-        id="basic-button"
-        aria-controls={a ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={a ? "true" : undefined}
-        onClick={handleClick}
-      >
-        Dashboard
-      </Button>
-      <DropDown
-        width="300px"
-        height="300px"
-        open={open}
-        handleClose={handleClose}
-        anchorEl={anchorEl}
-      >
-        <MenuItem> Alisher</MenuItem>
-      </DropDown>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
