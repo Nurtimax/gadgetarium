44e71c84ebdf907cb81c9c1a84a8ccb0f1013270
Alisher <zumanovaliser814@gmail.com>
2023-01-09 17:17:53 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import AboutStore from "./pages/landing/AboutStore";
+import "./App.css";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app">
+      <AboutStore />
+    </div>
+  );
 };
 
 export default App;
