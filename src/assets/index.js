576a176d135beb8eb82229db3aae70df5b495bf9
Alisher <zumanovaliser814@gmail.com>
2023-01-17 20:45:54 +0600

    create tables

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-export { ReactComponent as CartIcon } from "../assets/icons/cardicon.svg";
+// export { ReactComponent as CartIcon } from "../assets/icons/cardicon.svg";
 export { ReactComponent as ClockIcon } from "./icons/clock.svg";
 export { ReactComponent as Logo } from "./icons/logo.svg";
 export { ReactComponent as Phone } from "./icons/numberPhone.svg";
@@ -7,7 +7,7 @@ export { ReactComponent as Locotion } from "./icons/locotion.svg";
 export { ReactComponent as IconClose } from "../assets/icons/closeIconPopUp.svg";
 export { ReactComponent as SearchIcon } from "./icons/search.svg";
 export { ReactComponent as ArrowLeftIcon } from "./icons/arrows/arrowLeftPink.svg";
-export { ReactComponent as ImageKyrgyzstanKart } from "../assets/images/kyrgyzstanMap.svg";
+// export { ReactComponent as ImageKyrgyzstanKart } from "../assets/images/kyrgyzstanMap.svg";
 export { ReactComponent as IconNext } from "./icons/arrows/arrowRightWhiteBackround.svg";
 export { ReactComponent as IconPrev } from "./icons/arrows/arrowLeftWhiteBackround.svg";
 export { ReactComponent as DeliveryIcon } from "./icons/Pickup.svg";
