f69fcaf4ecf467a29452b5424d7bf4470da43ecc
Nurtimax <maksatbeknurtilek@gmail.com>
2023-02-07 16:05:04 +0600

    add some icons for brand and resolved some product

--- src/containers/add-product/AddProduct.jsx
+++ src/containers/add-product/AddProduct.jsx
@@ -6,17 +6,24 @@ import StepLabel from "@mui/material/StepLabel";
 import Typography from "@mui/material/Typography";
 import {
   Container,
-  FormControl,
   Grid,
+  IconButton,
   MenuItem,
   Select,
   styled,
 } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
-import { ArrowDownIcon, DownloadBannerIcon } from "../../assets";
+import {
+  ArrowDownIcon,
+  ChooseColorIcon,
+  DownloadBannerIcon,
+  PlusIcon,
+} from "../../assets";
 import Input from "../../components/UI/input/Input";
 import { catalogMenu_FAKE_DATA } from "../../utils/constants";
 import { useState } from "react";
+import Button from "../../components/UI/button/Button";
+import { PRODUCTBRAND } from "../../utils/constants/add-product";
 
 const steps = [
   "Добавление товара",
@@ -59,7 +66,7 @@ const AddProduct = () => {
           })}
         </Stepper>
         <React.Fragment>
-          <StyledFormControl size="small">
+          <StyledFormControl component="form" size="small">
             <Grid container spacing={2.5}>
               <Grid item xs={3.5}>
                 <Typography component="p" variant="body1">
@@ -140,11 +147,15 @@ const AddProduct = () => {
                     </Typography>
                   )}
                 >
-                  {chooseCatalog.subcategory.map((catalog) => (
+                  {PRODUCTBRAND.map((catalog) => (
                     <StyledMenuItem key={catalog.id}>
-                      {catalog.title}
+                      <IconButton size="small">{catalog.icon}</IconButton>
+                      {catalog.name}
                     </StyledMenuItem>
                   ))}
+                  <StyledButton className="create_product" variant="text">
+                    <PlusIcon /> Создать новый бренд
+                  </StyledButton>
                 </Select>
               </Grid>
               <Grid item xs={6}>
@@ -159,7 +170,134 @@ const AddProduct = () => {
                 </Typography>
                 <StyledInput placeholder="Введите название товара" />
               </Grid>
-              <Grid item xs={12}></Grid>
+              <Grid item xs={12} display="flex" gap="10px">
+                <StyledButton variant="outlined" className="product_button">
+                  Product 1
+                </StyledButton>
+                <StyledButton className="create_product" variant="text">
+                  <PlusIcon /> Добавить продукт
+                </StyledButton>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography component="p" variant="body1">
+                  Основной цвет
+                </Typography>
+                <StyledInput
+                  placeholder="Основной цвет"
+                  endAdornment={<ChooseColorIcon />}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Typography component="p" variant="body1">
+                  Объем памяти
+                </Typography>
+                <Select
+                  displayEmpty
+                  IconComponent={() => <ArrowDownIcon width={23} height={23} />}
+                  input={<Input />}
+                  renderValue={() => (
+                    <Typography
+                      variant="body1"
+                      component="span"
+                      className="placeholder"
+                    >
+                      Выберите объем памяти
+                    </Typography>
+                  )}
+                >
+                  {[2, 4, 8, 12, 16, 32].map((catalog) => (
+                    <StyledMenuItem key={catalog.id}>
+                      {catalog.title}
+                    </StyledMenuItem>
+                  ))}
+                </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography component="p" variant="body1">
+                  Оперативная память
+                </Typography>
+                <Select
+                  displayEmpty
+                  IconComponent={() => <ArrowDownIcon width={23} height={23} />}
+                  input={<Input />}
+                  renderValue={() => (
+                    <Typography
+                      variant="body1"
+                      component="span"
+                      className="placeholder"
+                    >
+                      Выберите оперативную память
+                    </Typography>
+                  )}
+                >
+                  {[2, 4, 8, 12, 16, 32].map((catalog) => (
+                    <StyledMenuItem key={catalog.id}>
+                      {catalog.title}
+                    </StyledMenuItem>
+                  ))}
+                </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography component="p" variant="body1">
+                  Кол-во SIM-карт
+                </Typography>
+                <Select
+                  displayEmpty
+                  IconComponent={() => <ArrowDownIcon width={23} height={23} />}
+                  input={<Input />}
+                  renderValue={() => (
+                    <Typography
+                      variant="body1"
+                      component="span"
+                      className="placeholder"
+                    >
+                      Выберите SIM-карты
+                    </Typography>
+                  )}
+                >
+                  {[2, 4, 8, 12, 16, 32].map((catalog) => (
+                    <StyledMenuItem key={catalog.id}>
+                      {catalog.title}
+                    </StyledMenuItem>
+                  ))}
+                </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography component="p" variant="body1">
+                  Добавьте фото
+                </Typography>
+                <StyledUploadImages>
+                  <Grid container>
+                    <Grid item xs={12} className="flex center upload_icon">
+                      <DownloadBannerIcon width={36} height={33} />
+                    </Grid>
+                    <Grid xs={12} className="upload_img">
+                      <Typography
+                        component="div"
+                        variant="body1"
+                        className="flex center"
+                      >
+                        Нажмите или перетащите сюда файл
+                      </Typography>
+                      <Typography
+                        className="flex center column flex-end lists"
+                        component="ul"
+                        variant="body1"
+                      >
+                        <Typography component="li">
+                          Минимальное разрешение - 450x600
+                        </Typography>
+                        <Typography component="li">
+                          максимальное количество - 10 фото
+                        </Typography>
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </StyledUploadImages>
+              </Grid>
+              <Grid item xs={3.5} display="grid">
+                <StyledButton className="next_button">Далее</StyledButton>
+              </Grid>
             </Grid>
           </StyledFormControl>
         </React.Fragment>
@@ -205,7 +343,8 @@ const StyledAddProduct = styled(Box)(({ theme }) => ({
   },
 }));
 
-const StyledFormControl = styled(FormControl)(() => ({
+const StyledFormControl = styled(Box)(() => ({
+  padding: "60px 0 140px",
   "& .MuiInputBase-root": {
     width: "396px",
     height: "39px",
@@ -231,6 +370,7 @@ const StyledFormControl = styled(FormControl)(() => ({
 const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
   width: "97%",
   margin: "0 auto",
+  color: "black",
   "&.MuiMenuItem-root:hover": {
     background: theme.palette.secondary.main,
     borderRadius: "11px",
@@ -240,4 +380,59 @@ const StyledMenuItem = styled(MenuItem)(({ theme }) => ({
 
 const StyledInput = styled(Input)(() => ({
   padding: 0,
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  "&.next_button": {
+    background: theme.palette.secondary.main,
+    color: "#fff !important",
+    width: "99px",
+    height: "43px",
+    justifySelf: "flex-end",
+  },
+  "&.product_button": {
+    padding: ".5rem",
+    width: "107px",
+    height: "35px",
+    color: `${theme.palette.grey[800]} !important`,
+    borderColor: `${theme.palette.grey[800]} !important`,
+    "&:hover": {
+      background: "none",
+    },
+  },
+  "&.create_product": {
+    width: "201px",
+    height: "35px",
+    color: `${theme.palette.secondary.main} !important`,
+    fontFamily: "Inter",
+    fontWeight: 500,
+    fontSize: "16px",
+    lineHeight: "19px",
+    display: "flex",
+    gap: 6,
+    background: "inherit",
+  },
+}));
+
+const StyledUploadImages = styled(Box)(({ theme }) => ({
+  width: "396px",
+  height: "168px",
+  background: "#D2D4D880",
+  border: "1px dashed",
+  borderRadius: "4px",
+  color: theme.palette.primary.dark,
+  "& .upload_icon": {
+    height: "75px",
+    color: "inherit",
+  },
+  "& .lists": {
+    fontSize: "12px",
+    width: "90%",
+    justifySelf: "flex-end",
+    color: "inherit",
+  },
+  "& .upload_img": {
+    display: "grid",
+    color: "inherit",
+  },
 }));
---
+++ src/utils/constants/add-product.js
@@ -0,0 +1,15 @@
+import {
+  AppleIcon,
+  HonorIcon,
+  HuaweiIcon,
+  SamsungIcon,
+  XiaomiIcon,
+} from "../../assets";
+
+export const PRODUCTBRAND = [
+  { id: 1, icon: <SamsungIcon />, name: "Samsung" },
+  { id: 2, icon: <AppleIcon />, name: "Apple" },
+  { id: 3, icon: <HuaweiIcon />, name: "Huawei" },
+  { id: 4, icon: <HonorIcon />, name: "Honor" },
+  { id: 5, icon: <XiaomiIcon />, name: "Xiaomi" },
+];
