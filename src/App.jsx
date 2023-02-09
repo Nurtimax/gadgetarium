fc655938f1f73a53b818fc924df473343415e6fa
Albina <azamatovaalbina7@gmail.com>
2023-02-09 17:43:15 +0600

    clear App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import PaymentCard from "./components/payment-card/PaymentCard";
 import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
     <div>
       <AppRoutes />
-      <PaymentCard />
     </div>
   );
 };
