d8201a94c63d042effb137b0eebf5e6330daf38b
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-04 18:58:03 +0600

    change logic and add brand image and brand name from store

--- src/components/add_product/table/BrandTableItem.jsx
+++ src/components/add_product/table/BrandTableItem.jsx
@@ -1,12 +1,39 @@
-import { Box, styled } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const BrandTableItem = () => {
-  return <StyledBrandTableItem>154665884</StyledBrandTableItem>;
+  const { editData, Productbrand } = useSelector((state) => state.addProduct);
+  const findedBrandItem = Productbrand.find(
+    (product) => product.id === editData.brand
+  );
+  return (
+    <StyledBrandTableItem>
+      <Box className="brand_image">
+        <img src={findedBrandItem?.image} alt="" />
+      </Box>
+      <Typography>{findedBrandItem?.brandName}</Typography>
+    </StyledBrandTableItem>
+  );
 };
 
 export default BrandTableItem;
 
 const StyledBrandTableItem = styled(Box)(() => ({
-  padding: "20px",
+  padding: "14px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  "& .MuiTypography-root": {
+    padding: 0,
+  },
+  "& .brand_image": {
+    "& img": {
+      width: "20px",
+      height: "20px",
+      objectFit: "contain",
+      mixBlendMode: "color-burn",
+    },
+  },
 }));
