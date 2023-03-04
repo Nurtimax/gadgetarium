26861354faed096dba9abaaea3ea8206abfc716b
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-04 20:18:48 +0600

    change date now

--- src/components/add_product/table/DateTableItem.jsx
+++ src/components/add_product/table/DateTableItem.jsx
@@ -1,12 +1,25 @@
-import { Box, styled } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
+import { format } from "date-fns";
 import React from "react";
+import { useSelector } from "react-redux";
 
-const DateTableItem = () => {
-  return <StyledDateTableItem>12.12.2020</StyledDateTableItem>;
+const DateTableItem = ({ id }) => {
+  const { editData } = useSelector((state) => state.addProduct);
+  const date = format(editData.date[id], "dd.MM.yyyy");
+  const hour = format(editData.date[id], "HH:mm");
+  return (
+    <StyledDateTableItem>
+      <Typography classes={{ root: "date_text" }}>{date}</Typography>
+      <Typography classes={{ root: "date_text" }}> {hour}</Typography>
+    </StyledDateTableItem>
+  );
 };
 
 export default DateTableItem;
 
 const StyledDateTableItem = styled(Box)(() => ({
-  padding: "20px",
+  padding: "15px",
+  "& .date_text": {
+    padding: 0,
+  },
 }));
