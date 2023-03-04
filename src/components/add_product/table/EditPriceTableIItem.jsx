1b58a70c670e42ca9a110475e8b3dfe5b80cea0d
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-04 20:19:38 +0600

    change edit price logic

--- src/components/add_product/table/EditPriceTableIItem.jsx
+++ src/components/add_product/table/EditPriceTableIItem.jsx
@@ -4,23 +4,32 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ActionAddProductSlice } from "../../../redux/slices/add-product";
 
-const EditPriceTableIItem = () => {
+const EditPriceTableIItem = ({ id, price }) => {
   const { editData } = useSelector((state) => state.addProduct);
-  const [total, setTotal] = useState(editData.price);
+  const [total, setTotal] = useState(price);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    setTotal(editData.price);
-  }, [editData]);
+    setTotal(price);
+  }, [price]);
 
   const editPriceHandler = () => {
     dispatch(
-      ActionAddProductSlice.editData({ isChecked: "price", price: total })
+      ActionAddProductSlice.editData({
+        ...editData,
+        isChecked: "price",
+        price: Number(total),
+        id,
+      })
     );
   };
 
   return (
-    <StyledEditPriceTableItem onClick={editPriceHandler}>
+    <StyledEditPriceTableItem
+      onClick={editPriceHandler}
+      className={editData.errorPriceId.includes(Number(id)) ? "error" : ""}
+    >
       {total}
     </StyledEditPriceTableItem>
   );
@@ -29,6 +38,9 @@ const EditPriceTableIItem = () => {
 export default EditPriceTableIItem;
 
 const StyledEditPriceTableItem = styled(Box)(({ theme }) => ({
-  padding: "20px",
+  padding: "28.5px",
   background: theme.palette.secondary.main + "31",
+  "&.error": {
+    border: "1px solid red",
+  },
 }));
