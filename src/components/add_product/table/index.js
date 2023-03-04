7b39d21993040bedbb013939e6799355cf342571
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-04 20:21:28 +0600

    slices object for table data

--- src/components/add_product/table/index.js
+++ src/components/add_product/table/index.js
@@ -1,12 +1,47 @@
-import React from "react";
-import { ADD_PRODUCT_TABLE_HEADER_TITLE } from "../../../utils/constants/add-product";
+import React, { useMemo } from "react";
+import {
+  ADD_PRODUCT_TABLE_HEADER_TITLE,
+  ChangeValueTableData,
+  tableReusibleItems,
+} from "../../../utils/constants/add-product";
 import Table from "../../Table";
+import Charactersitics from "./Charactersitics";
 
-const AddProductTable = ({ tableData = [] }) => {
+const AddProductTable = ({ tableData = [], id }) => {
+  const changeReusibleTableItem = useMemo(() => {
+    const findedTableItem = tableReusibleItems.find(
+      (tableItem) => tableItem.id === id
+    );
+
+    return findedTableItem.keys.map((item) => {
+      return {
+        Header: item.name,
+        accessor: item.key,
+        style: {
+          flex: item.flex,
+        },
+        Cell: ({ row }) => {
+          return (
+            <Charactersitics
+              item={row.original.characteristics}
+              id={item.key}
+            />
+          );
+        },
+      };
+    });
+  }, [id]);
+
+  const tableHeader = [
+    ...ADD_PRODUCT_TABLE_HEADER_TITLE,
+    ...changeReusibleTableItem,
+    ...ChangeValueTableData,
+  ];
+
   return (
     <div>
       <Table
-        tableHeaderTitle={ADD_PRODUCT_TABLE_HEADER_TITLE}
+        tableHeaderTitle={tableHeader}
         data={tableData}
         isMarked={null}
         found={false}
