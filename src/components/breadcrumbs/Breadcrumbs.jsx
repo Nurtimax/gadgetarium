2473c72000b1251784a37dd321f34cc530c0a8e2
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-08 18:11:55 +0600

    add admin breadcrumbs

--- src/components/breadcrumbs/Breadcrumbs.jsx
+++ src/components/breadcrumbs/Breadcrumbs.jsx
@@ -8,6 +8,23 @@ import { BREADCRUMBS } from "../../utils/constants/breadcrumbs";
 const BreadCrumbs = () => {
   const breadcrumbs = useBreadcrumbs(BREADCRUMBS);
 
+  const adminBreadcrumbs = breadcrumbs.filter((breadcrumb) =>
+    breadcrumb.key.split("/").includes("admin")
+  );
+
+  if (adminBreadcrumbs.length !== 0) {
+    return (
+      <Breadcrumbs separator={<KeyboardDoubleArrowRightIcon fontSize="6px" />}>
+        {adminBreadcrumbs.length > 1 &&
+          adminBreadcrumbs.map(({ match, breadcrumb }) => (
+            <NavLink key={match.pathname} to={match.pathname}>
+              {breadcrumb}
+            </NavLink>
+          ))}
+      </Breadcrumbs>
+    );
+  }
+
   return (
     <Breadcrumbs separator={<KeyboardDoubleArrowRightIcon fontSize="6px" />}>
       {breadcrumbs.length > 1 &&
