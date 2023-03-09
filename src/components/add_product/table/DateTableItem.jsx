94d7be57ab4a0c060862a5b34ee771d5c041005a
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-09 13:13:29 +0600

    remove typography and change padding size

--- src/components/add_product/table/DateTableItem.jsx
+++ src/components/add_product/table/DateTableItem.jsx
@@ -1,4 +1,4 @@
-import { Box, styled, Typography } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import { format } from "date-fns";
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
@@ -12,18 +12,11 @@ const DateTableItem = () => {
     return null;
   }, [editData.date]);
 
-  return (
-    <StyledDateTableItem>
-      <Typography classes={{ root: "date_text" }}>{date}</Typography>
-    </StyledDateTableItem>
-  );
+  return <StyledDateTableItem>{date}</StyledDateTableItem>;
 };
 
 export default DateTableItem;
 
 const StyledDateTableItem = styled(Box)(() => ({
-  padding: "15px",
-  "& .date_text": {
-    padding: 0,
-  },
+  padding: "28.5px",
 }));
