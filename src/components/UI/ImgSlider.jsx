405fadf95a4584ade116d873311d1bac8e2b6b0d
kaamilov <kaamilovmalik@gmail.com>
2023-03-10 17:54:38 +0300

    change img slider

--- src/components/UI/ImgSlider.jsx
+++ src/components/UI/ImgSlider.jsx
@@ -1,14 +1,31 @@
 import { styled } from "@mui/material";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-
+import { useState } from "react";
+import DetailsImg from "../product-details/DetailsImg";
 const ImgSlider = ({ images }) => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const open = () => {
+    setOpenModal(true);
+  };
+  const close = () => {
+    setOpenModal(false);
+  };
   return (
     <div>
+      {openModal && (
+        <DetailsImg
+          state={openModal}
+          open={open}
+          handleClose={close}
+          images={images}
+        />
+      )}
       <CarouselStyle infiniteLoop showStatus={false} showIndicators={false}>
         {images?.map((img) => (
-          <div key={img.id}>
-            <img src={img.url} alt={img.id} />
+          <div key={img} onClick={open}>
+            <img src={img} alt="" />
           </div>
         ))}
       </CarouselStyle>
@@ -18,20 +35,36 @@ const ImgSlider = ({ images }) => {
 
 export default ImgSlider;
 
-const CarouselStyle = styled(Carousel)`
-  .carousel {
-    display: flex;
-    justify-content: center;
-    padding-top: 100px;
-  }
-  .carousel .slide img {
-    width: 310px;
-    height: 364px;
-  }
-  .thumbs .selected {
-    border: 1px solid #cb11ab;
-  }
-  .thumb:hover {
-    border: 1px solid #cb11ab;
-  }
-`;
+const CarouselStyle = styled(Carousel)(() => ({
+  "& .carousel-slider": {
+    display: "flex",
+    justifyContent: "center",
+  },
+  "& .thumb": {
+    border: "none",
+  },
+  "& .thumb:hover": {
+    border: "2px solid #cb11ab",
+  },
+
+  "& .slider-wrapper": {
+    "& img": {
+      width: "300px",
+      height: "350px",
+      aspectRatio: "1/1",
+      objectFit: "contain",
+    },
+  },
+
+  "& .thumbs": {
+    display: "flex",
+    justifyContent: "center",
+    "& .selected": {
+      border: "2px solid #cb11ab",
+    },
+  },
+
+  "& button": {
+    display: "none",
+  },
+}));
