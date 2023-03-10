9677a17d40297b85a4f0dcda0aab54526aad40c0
kaamilov <kaamilovmalik@gmail.com>
2023-03-10 18:28:26 +0300

    there was a conflict

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import CharacteristicsTabItem from "../components/product-details/CharacteristicsTabItem";
+import DescriptionTabItem from "../components/product-details/DescriptionTabItem";
+import ReviewsTabItem from "../components/product-details/ReviewsTabItem";
 import AboutStore from "../containers/about-store/AboutStore";
 import CatalogProducts from "../containers/catalog-products/CatalogProducts";
 import Contacts from "../containers/contacts/Contacts";
@@ -7,6 +10,7 @@ import Delivery from "../containers/delivery/Delivery";
 import FrequentlyAskedQuestions from "../containers/FAQ/FrequentlyAskedQuestions";
 import Home from "../containers/home";
 import OrderPage from "../containers/order-page/OrderPage";
+import MainProductDetails from "../containers/productDetails/MainProductDetails";
 import SignIn from "../containers/sign-in/SignIn";
 import SignUp from "../containers/sign-up/Signup";
 import Layout from "../layout";
@@ -29,11 +33,16 @@ const MainRoutes = () => {
           path={`item/${ROUTES.PHONE}/${ROUTES.PRODUCT}`}
           element={
             <PrivateRoute
-              Component={<h1>product item by id</h1>}
+              Component={<MainProductDetails />}
               role={["Customer"]}
             />
           }
-        />
+        >
+          <Route path="description" element={<DescriptionTabItem />} />
+          <Route path="characteristics" element={<CharacteristicsTabItem />} />
+          <Route path="reviews" element={<ReviewsTabItem />} />
+          <Route path="shipping-and-payment" element={<Delivery />} />
+        </Route>
         <Route path={ROUTES.CART} element={<h1>Товары в корзине</h1>} />
         <Route path={ROUTES.CHECKOUT} element={<OrderPage />} />
         <Route path={ROUTES.COMPATISONPRODUCT} element={<h1>comparative</h1>} />
@@ -41,7 +50,6 @@ const MainRoutes = () => {
         <Route path={ROUTES.DELIVERY} element={<Delivery />} />
         <Route path={ROUTES.FAG} element={<FrequentlyAskedQuestions />} />
         <Route path={ROUTES.CONTACTS} element={<Contacts />} />
-
         <Route path={ROUTES.VIP} element={<Navigate to={ROUTES.HISTORY} />}>
           <Route path={ROUTES.HISTORY} element={<h1>History</h1>} />
           <Route path={ROUTES.PROFILE} element={<h1>Profile</h1>} />
