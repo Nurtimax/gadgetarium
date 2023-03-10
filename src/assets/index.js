cd0fcd1f77d0e05732ff6da35f8bc5779696ff2f
kaamilov <kaamilovmalik@gmail.com>
2023-03-10 17:39:48 +0300

    add accordion arrow icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -51,3 +51,4 @@ export { ReactComponent as MasterCard } from "./icons/masterCard.svg";
 export { ReactComponent as VisaIcon } from "./icons/visa.svg";
 export { ReactComponent as DetailLogo } from "./icons/detailTitle.svg";
 export { ReactComponent as ArrowUpIcon } from "./icons/arrows/arrowUp.svg";
+export { ReactComponent as ArrowAccordion } from "./icons/arrowAccordion.svg";
