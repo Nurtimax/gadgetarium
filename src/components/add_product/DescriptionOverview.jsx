a66035de87f07d5151ebff8a90bcbb6643e92f57
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-11 19:31:12 +0600

    removed buttons and added sending and deleting details and caused values and errors from the repository and added key changes across the store and created a function, to change the description and still for PDF file also and gave permission only for PDF file and made errors visible and deleted buttons

--- src/components/add_product/DescriptionOverview.jsx
+++ src/components/add_product/DescriptionOverview.jsx
@@ -2,26 +2,25 @@ import { Box, Grid, styled, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { DownloadVideoIcon } from "../../assets";
+import { ActionAddProductSlice } from "../../redux/slices/add-product-slice";
 import { SWAGGER_API } from "../../utils/constants/fetch";
 import GadgetariumSpinnerLoading from "../GadgetariumSpinnerLoading";
-import Button from "../UI/button/Button";
 import Input from "../UI/input/Input";
 import TextEditor from "../UI/TextEditor";
 
-const DescriptionOverview = ({
-  setFieldValue,
-  values,
-  handleChange,
-  handleSubmit,
-}) => {
+const DescriptionOverview = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const { isLoading: isSendDataLoading } = useSelector(
-    (state) => state.addProduct
-  );
+  const {
+    isLoading: isSendDataLoading,
+    values,
+    errors,
+  } = useSelector((state) => state.addProduct);
+
+  const dispatch = useDispatch();
 
   const getImageLinkHandler = async (file) => {
     setIsLoading(true);
@@ -35,7 +34,12 @@ const DescriptionOverview = ({
     })
       .then((response) => {
         if (response.data.link) {
-          setFieldValue("pdf", response.data.link);
+          dispatch(
+            ActionAddProductSlice.editValuesWithKey({
+              key: "pdf",
+              value: response.data.link,
+            })
+          );
           setIsLoading(false);
         }
       })
@@ -45,12 +49,28 @@ const DescriptionOverview = ({
       });
   };
 
+  const handleChangeDescription = (payload) => {
+    dispatch(ActionAddProductSlice.editValuesWithKey(payload));
+  };
+
+  const handleChangeVideoReview = (e) => {
+    dispatch(
+      ActionAddProductSlice.editValuesWithKey({
+        key: e.target.name,
+        value: e.target.value,
+      })
+    );
+  };
+
   const { getInputProps, getRootProps } = useDropzone({
     onDrop: getImageLinkHandler,
+    accept: {
+      "application/pdf": [],
+    },
   });
 
   return (
-    <StyledDescriptionOverview onSubmit={handleSubmit} component="form">
+    <StyledDescriptionOverview>
       {isSendDataLoading && <GadgetariumSpinnerLoading />}
       {isLoading && <GadgetariumSpinnerLoading />}
       <Grid container spacing={2.5}>
@@ -60,9 +80,14 @@ const DescriptionOverview = ({
             startAdornment={<DownloadVideoIcon />}
             placeholder="Вставьте ссылку на видеообзор"
             value={values.videoReview}
-            onChange={handleChange}
+            onChange={handleChangeVideoReview}
             name="videoReview"
           />
+          {Boolean(errors?.videoReview) && (
+            <Typography component="p" variant="body2" color="error">
+              {errors.videoReview}
+            </Typography>
+          )}
         </Grid>
         <Grid item {...getRootProps()}>
           <input {...getInputProps()} />
@@ -79,17 +104,14 @@ const DescriptionOverview = ({
               {error}
             </Typography>
           )}
+          {Boolean(errors?.pdf) && (
+            <Typography component="p" variant="body2" color="error">
+              {errors.pdf}
+            </Typography>
+          )}
         </Grid>
         <Grid item xs={12}>
-          <TextEditor onChange={setFieldValue} values={values} />
-        </Grid>
-        <Grid item xs={6.365} className="flex gap2 flex-end">
-          <StyledButton className="cancel_button" type="button">
-            Отменить
-          </StyledButton>
-          <StyledButton className="add_button" type="submit">
-            Добавить
-          </StyledButton>
+          <TextEditor onChange={handleChangeDescription} />
         </Grid>
       </Grid>
     </StyledDescriptionOverview>
@@ -105,21 +127,4 @@ const StyledInput = styled(Input)(() => ({
 
   display: "flex",
   gap: "12.5px",
-}));
-const StyledButton = styled(Button)(({ theme }) => ({
-  width: "134px",
-  height: "43px",
-  fontSize: "1rem",
-  fontWeight: 600,
-  "&.cancel_button": {
-    border: `1px solid ${theme.palette.secondary.main}`,
-    "&:hover": {
-      background: "white",
-      color: theme.palette.secondary.main,
-    },
-  },
-  "&.add_button": {
-    background: theme.palette.secondary.main,
-    color: "white",
-  },
 }));
