8388950ab04b2dfd6416ba28ae4de9b18e0b7b27
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-11 20:07:26 +0600

    change component in lazy method and add suspense component

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -2,7 +2,6 @@ import { lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import GadgetariumSpinnerLoading from "../components/GadgetariumSpinnerLoading";
 import React from "react";
-import OrderItem from "../components/orders/OrderItem";
 import { ROUTES } from "../utils/constants/routes";
 
 const AddProduct = lazy(() => import("../containers/add-product/AddProduct"));
@@ -10,6 +9,14 @@ const Orders = lazy(() => import("../containers/orders/Orders"));
 const AdminLayout = lazy(() => import("../layout/admin"));
 const Goods = lazy(() => import("../containers/goods"));
 const ReviewRating = lazy(() => import("../containers/review-rating"));
+const OrderItem = lazy(() => import("../components/orders/OrderItem"));
+const Forms = lazy(() => import("../components/add_product/Forms"));
+const PriceQuantity = lazy(() => {
+  return import("../components/add_product/PriceQuantity");
+});
+const DescriptionOverview = lazy(() => {
+  return import("../components/add_product/DescriptionOverview");
+});
 
 const AdminRoutes = () => {
   return (
@@ -47,7 +54,32 @@ const AdminRoutes = () => {
               <AddProduct />
             </Suspense>
           }
-        />
+        >
+          <Route
+            path="part-1"
+            element={
+              <Suspense fallback={<GadgetariumSpinnerLoading />}>
+                <Forms />
+              </Suspense>
+            }
+          />
+          <Route
+            path="part-2"
+            element={
+              <Suspense fallback={<GadgetariumSpinnerLoading />}>
+                <PriceQuantity />
+              </Suspense>
+            }
+          />
+          <Route
+            path="part-3"
+            element={
+              <Suspense fallback={<GadgetariumSpinnerLoading />}>
+                <DescriptionOverview />
+              </Suspense>
+            }
+          />
+        </Route>
         <Route
           path={`${ROUTES.ORDERS}`}
           element={
@@ -57,8 +89,22 @@ const AdminRoutes = () => {
           }
         />
 
-        <Route path={`${ROUTES.ORDERS}`} element={<Orders />} />
-        <Route path={`${ROUTES.ORDERS}/:orderId`} element={<OrderItem />} />
+        <Route
+          path={`${ROUTES.ORDERS}`}
+          element={
+            <Suspense fallback={<GadgetariumSpinnerLoading />}>
+              <Orders />
+            </Suspense>
+          }
+        />
+        <Route
+          path={`${ROUTES.ORDERS}/:orderId`}
+          element={
+            <Suspense fallback={<GadgetariumSpinnerLoading />}>
+              <OrderItem />
+            </Suspense>
+          }
+        />
 
         <Route
           path={`${ROUTES.REVIEWSRATING}`}
