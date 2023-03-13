bf35b43499ca0692f586f6bc444cce7c64217ac6
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-13 18:00:26 +0600

    create redux slice for viewed goods

--- src/redux/slices/viewed-product-slice.js
+++ src/redux/slices/viewed-product-slice.js
@@ -5,12 +5,7 @@ export const fetchViewedProductThunk = createAsyncThunk(
   "viewedProductSlice/fetchViewedProductThunk",
   async (size, { rejectWithValue }) => {
     try {
-      const { data, status } = await axiosInstance.get(`products/viewed`, {
-        params: {
-          page: 1,
-          size,
-        },
-      });
+      const { data, status } = await axiosInstance.get(`products/viewed`);
       if (!status === 200) {
         throw new Error("Server orror");
       }
@@ -22,7 +17,14 @@ export const fetchViewedProductThunk = createAsyncThunk(
 );
 
 const initialState = {
-  data: {},
+  data: {
+    viewedProducts: [],
+  },
+  errors: {
+    viewedProductError: null,
+    message: null,
+  },
+  isLoading: false,
 };
 
 const viewedProductSlice = createSlice({
@@ -31,9 +33,25 @@ const viewedProductSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchViewedProductThunk.fulfilled, () => {})
-      .addCase(fetchViewedProductThunk.pending, () => {})
-      .addCase(fetchViewedProductThunk.rejected, () => {});
+      .addCase(fetchViewedProductThunk.fulfilled, (state, action) => {
+        if (Array.isArray(action.payload)) {
+          state.data.viewedProducts = action.payload;
+          state.errors = null;
+        } else {
+          state.errors.viewedProductError =
+            "Простите пожалуста, что то не так с данными или с серверам";
+        }
+        state.isLoading = false;
+      })
+      .addCase(fetchViewedProductThunk.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchViewedProductThunk.rejected, (state, action) => {
+        if (action.payload.message) {
+          state.errors.message = action.payload.message;
+        }
+        state.isLoading = false;
+      });
   },
 });
 
