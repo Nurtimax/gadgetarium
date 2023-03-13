4a8b80839d6559ed7e2eb7bd6c4bbc45c9b2e106
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-13 17:56:56 +0600

    Called the goods and together caused errors and put conditions

--- src/components/viewed-products/ViewedProducts.jsx
+++ src/components/viewed-products/ViewedProducts.jsx
@@ -1,20 +1,40 @@
 import { Box, Grid, styled, Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import GadgetariumSpinnerLoading from "../GadgetariumSpinnerLoading";
 import ViewedProductCard from "../UI/card/ViewedProductCard";
 
 const ViewedProducts = () => {
+  const { data, isLoading, errors } = useSelector((state) => state.viewed);
+
+  if (isLoading) {
+    return <GadgetariumSpinnerLoading />;
+  }
+
   return (
     <StyledViewedProduct>
       <Typography classes={{ root: "typography" }} variant="h5">
         Просмотренные товары
       </Typography>
-      <Grid container spacing={1}>
-        {[1, 2, 3, 4, 5].map((item) => (
-          <Grid item key={item}>
-            <ViewedProductCard />
-          </Grid>
-        ))}
-      </Grid>
+      {errors?.viewedProductError ? (
+        <Typography classes={{ root: "typography" }} variant="h5">
+          {errors.viewedProductError}
+        </Typography>
+      ) : (
+        <>
+          {!data?.viewedProducts.length ? (
+            <Typography variant="h5">Нет продуктов</Typography>
+          ) : (
+            <Grid container spacing={1}>
+              {data?.viewedProducts?.map((product) => (
+                <Grid item key={product.productId}>
+                  <ViewedProductCard {...product} />
+                </Grid>
+              ))}
+            </Grid>
+          )}
+        </>
+      )}
     </StyledViewedProduct>
   );
 };
