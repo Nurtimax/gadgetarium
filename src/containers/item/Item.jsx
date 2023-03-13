a638dd99a6b6446f39f9831c816629b6f315b54b
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-13 17:58:33 +0600

    get reviewed goods

--- src/containers/item/Item.jsx
+++ src/containers/item/Item.jsx
@@ -1,9 +1,18 @@
 import { Container } from "@mui/material";
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import ViewedProducts from "../../components/viewed-products/ViewedProducts";
+import { fetchViewedProductThunk } from "../../redux/slices/viewed-product-slice";
 
 const Item = () => {
+  const dipatch = useDispatch();
+
+  useEffect(() => {
+    dipatch(fetchViewedProductThunk());
+  }, []);
+
   return (
     <>
       <Outlet />
