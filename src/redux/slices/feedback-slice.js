228523f51fa46913eb41f7c41552e34a8554faff
kaamilov <kaamilovmalik@gmail.com>
2023-03-21 00:56:55 +0300

    create slice and change slice

--- src/redux/slices/feedback-slice.js
+++ src/redux/slices/feedback-slice.js
@@ -1,17 +1,77 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axiosInstance from "../../config/axios-instance";
+import { getProductDetailThunk } from "./product-details";
+import { toast } from "react-toastify";
 const initialState = {
-  data: {},
   status: null,
   error: null,
 };
 export const postAddFeedback = createAsyncThunk(
   "addFeedbackSlice/postAddFeedback",
-  async (data, { rejectWithValue }) => {
-    // console.log(data);
+  async (params, { rejectWithValue, dispatch }) => {
     try {
-      const response = await axiosInstance.post("users/review", data);
+      const response = await axiosInstance.post("users/review", params);
+      console.log(response);
       const result = response.data;
+
+      if (response.status === 200) {
+        toast.success("Вы успешно оставили отзыв");
+        dispatch(
+          getProductDetailThunk({
+            product: params.product,
+            attribute: "Отзывы",
+          })
+        );
+      }
+
+      return result;
+    } catch (error) {
+      if (rejectWithValue) {
+        return error;
+      }
+      return error;
+    }
+  }
+);
+export const putAddFeedback = createAsyncThunk(
+  "addFeedbackSlice/putAddFeedback",
+  async (params, { rejectWithValue, dispatch }) => {
+    try {
+      const response = await axiosInstance.put("users/review", params.data);
+      const result = response.data;
+      if (response.status === 200) {
+        toast.success("Ваш отзыв был успешно отредактирован");
+        dispatch(
+          getProductDetailThunk({
+            product: params.product,
+            attribute: "Отзывы",
+          })
+        );
+      }
+
+      return result;
+    } catch (error) {
+      if (rejectWithValue) {
+        return error;
+      }
+      return error;
+    }
+  }
+);
+export const deletetFeedback = createAsyncThunk(
+  "addFeedbackSlice/deletetFeedback",
+  async ({ reviewid, product }, { rejectWithValue, dispatch }) => {
+    try {
+      const response = await axiosInstance.delete(`users/revie`, {
+        params: { review: reviewid },
+      });
+      const result = response.data;
+      if (response.status === 200) {
+        toast.success("Вы  удалили отзыв");
+
+        dispatch(getProductDetailThunk({ product, attribute: "Отзывы" }));
+      }
+
       return result;
     } catch (error) {
       if (rejectWithValue) {
@@ -30,11 +90,11 @@ const addFeedbackSlice = createSlice({
       state.status = "pending";
     });
     builder.addCase(postAddFeedback.fulfilled, (state, action) => {
-      console.log(action.payload.response.data.message);
       state.status = "success";
-      state.data = action.payload;
+      state.successMessage = action.payload;
     });
     builder.addCase(postAddFeedback.rejected, (state, action) => {
+      console.log(action.payload);
       state.status = "error";
       state.error = action.payload;
     });
