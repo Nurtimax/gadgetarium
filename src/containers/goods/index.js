50e8fe5952b2e86018a40d56adedf4d0056860b6
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-21 15:54:41 +0600

    remove and create some function

--- src/containers/goods/index.js
+++ src/containers/goods/index.js
@@ -1,13 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Box, Container, Grid, styled } from "@mui/material";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AddProductButton from "../../components/goods-cmp/buttons/AddProductButton";
+import ContentDatePicker from "../../components/goods-cmp/content-date-picker/ContentDatePicker";
+import ContentTab from "../../components/goods-cmp/content-tab";
+import ContentTable from "../../components/goods-cmp/content-table/ContentTable";
+import SearchItem from "../../components/goods-cmp/search/SearchItem";
+import { getProductsThunk } from "../../redux/slices/goods-slice";
 
 const Goods = () => {
+  const { params } = useSelector((state) => state.goods);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (params.productType && params.page) {
+      dispatch(getProductsThunk(params));
+    }
+  }, [params]);
+
   return (
-    <div>
-      <h5>goods</h5>
-      <Link to="add-product/part-1">add product</Link>
-    </div>
+    <StyledGoods>
+      <Container>
+        <Grid container spacing={3} className="flex between">
+          <SearchItem />
+          <AddProductButton />
+          <ContentTab />
+          <ContentDatePicker />
+          <ContentTable />
+        </Grid>
+      </Container>
+    </StyledGoods>
   );
 };
 
 export default Goods;
+
+const StyledGoods = styled(Box)(() => ({}));
