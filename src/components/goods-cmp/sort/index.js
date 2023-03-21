79063ff5d22f62c5b4d023bc2a1c66b2ca51a3b7
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-21 15:50:21 +0600

    create sort content

---
+++ src/components/goods-cmp/sort/index.js
@@ -0,0 +1,28 @@
+import { Box, styled, Typography } from "@mui/material";
+import React from "react";
+import { ArrowDownIcon } from "../../../assets";
+import useDropDown from "../../../hooks/useDropDown";
+import Sort from "../../catalog-products/Sort";
+
+const SortContent = () => {
+  const [anchorEl, setAnchorEl] = useDropDown();
+
+  return (
+    <StyledSort className="pointer">
+      <Sort anchorElCatalog={anchorEl} handleCloseCatalog={setAnchorEl} />
+      <Box className="flex gap" onClick={setAnchorEl}>
+        <Typography>Сортировать</Typography>
+        <ArrowDownIcon />
+      </Box>
+    </StyledSort>
+  );
+};
+
+export default SortContent;
+
+const StyledSort = styled(Box)(() => ({
+  padding: "40px 0 16px",
+  "& .MuiTypography-root": {
+    padding: 0,
+  },
+}));
