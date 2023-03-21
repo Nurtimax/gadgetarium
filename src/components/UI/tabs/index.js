ba7aba0b280ab6c77a8c5aeb3b1f9efe5e64477e
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-21 15:53:16 +0600

    create tabs for goods component

---
+++ src/components/UI/tabs/index.js
@@ -0,0 +1,54 @@
+import { Box, styled } from "@mui/material";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Button from "../button/Button";
+
+const Tabs = ({ contents = [], onChange }) => {
+  const [isChecked, setIsChecked] = useState(1);
+
+  const handleChangeButtonTab = (id, label) => {
+    setIsChecked(id);
+    onChange(label);
+  };
+
+  useEffect(() => {
+    onChange(contents[0].label);
+  }, []);
+
+  return (
+    <StyledTabs>
+      {contents.map((tab) => (
+        <StyledButton
+          key={tab.id}
+          className={`${isChecked === tab.id ? "active" : ""}`}
+          onClick={() => handleChangeButtonTab(tab.id, tab.label)}
+        >
+          {tab.label}
+        </StyledButton>
+      ))}
+    </StyledTabs>
+  );
+};
+
+export default Tabs;
+
+const StyledTabs = styled(Box)(() => ({
+  display: "flex",
+  gap: "12px",
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  width: 127,
+  padding: "8px 20px",
+  color: `${theme.palette.primary.light} !important`,
+  background: "#E0E2E7",
+  fontWeight: 600,
+  fontSize: "16px",
+  "&:hover": {
+    background: "rgba(0,0,0,0.2)",
+  },
+  "&.active": {
+    background: `${theme.palette.primary.light} !important`,
+    color: "white !important",
+  },
+}));
