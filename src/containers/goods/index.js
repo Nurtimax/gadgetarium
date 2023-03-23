781780711ea9a9760cc75e9b3a53685bc1c7cde5
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-24 00:04:53 +0600

    remove mount function

--- src/containers/goods/index.js
+++ src/containers/goods/index.js
@@ -1,24 +1,15 @@
 import { Box, Container, Grid, styled } from "@mui/material";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useSearchParams } from "react-router-dom";
 import AddProductButton from "../../components/goods-cmp/buttons/AddProductButton";
 import ContentDatePicker from "../../components/goods-cmp/content-date-picker/ContentDatePicker";
 import ContentTab from "../../components/goods-cmp/content-tab";
 import ContentTable from "../../components/goods-cmp/content-table/ContentTable";
 import SearchItem from "../../components/goods-cmp/search/SearchItem";
-import {
-  actionGoodSlice,
-  getProductsThunk,
-} from "../../redux/slices/goods-slice";
+import { getProductsThunk } from "../../redux/slices/goods-slice";
 
 const Goods = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const {
-    params,
-    localParams,
-    localParamsKeys = [],
-  } = useSelector((state) => state.goods);
+  const { params } = useSelector((state) => state.goods);
 
   const dispatch = useDispatch();
 
@@ -27,20 +18,6 @@ const Goods = () => {
       dispatch(getProductsThunk(params));
     }
   }, [params]);
-
-  useEffect(() => {
-    const mountData = localParamsKeys.reduce((acc, current) => {
-      return {
-        ...acc,
-        [current]: searchParams.get(current) ? searchParams.get(current) : null,
-      };
-    }, {});
-    dispatch(actionGoodSlice.changeAllParams(mountData));
-  }, []);
-
-  useEffect(() => {
-    setSearchParams(localParams);
-  }, [localParams]);
 
   return (
     <StyledGoods>
