80f4e6a1a28989b2d37b08aad9c3d28a59427be9
Nurtimax <maksatbeknurtilek@gmail.com>
2023-03-23 23:58:41 +0600

    do validate  and change handler and  do toast success message

--- src/components/goods-cmp/create-discount/index.js
+++ src/components/goods-cmp/create-discount/index.js
@@ -1,5 +1,13 @@
 import { Box, FormLabel, styled, Typography } from "@mui/material";
+import { format } from "date-fns";
+import { useFormik } from "formik";
 import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { postDiscountThunk } from "../../../redux/slices/create-discount-slice";
+import { actionGoodSlice } from "../../../redux/slices/goods-slice";
+import validationSchema from "../../../utils/constants/discount";
 import DatePicker from "../../orders/DatePicker";
 import Button from "../../UI/button/Button";
 import Input from "../../UI/input/Input";
@@ -8,20 +16,91 @@ import Modal from "../../UI/Modal";
 const CreateDiscount = ({ openModal, setOpenModal }) => {
   const [dates, setDates] = useState([null, null]);
 
+  const { choosedItems, params } = useSelector((state) => state.goods);
+
+  const dispatch = useDispatch();
+
+  const { values, handleChange, setFieldValue, handleSubmit, errors } =
+    useFormik({
+      initialValues: {
+        amountOfDiscount: "",
+        startDate: "",
+        endDate: "",
+        productId: [],
+      },
+      validationSchema,
+      validateOnChange: false,
+      onSubmit: (values, action) => {
+        dispatch(postDiscountThunk({ data: values, params })).then((res) => {
+          if (res.payload.message !== "Network Error") {
+            if (res.payload.status === "ok") {
+              toast.success("Скидка успешно сохранена!");
+              setOpenModal();
+              action.resetForm();
+              dispatch(actionGoodSlice.resetChoosedProducts());
+            }
+          } else {
+            toast.error("Что-то не так с серверами или данными");
+          }
+        });
+      },
+    });
+
+  useEffect(() => {
+    if (dates[0]) {
+      const dateFormat = format(new Date(dates[0]), "yyyy-MM-dd");
+      setFieldValue("startDate", dateFormat);
+    }
+  }, [dates]);
+
+  useEffect(() => {
+    if (dates[1]) {
+      const dateFormat = format(new Date(dates[1]), "yyyy-MM-dd");
+      setFieldValue("endDate", dateFormat);
+    }
+  }, [dates]);
+
+  useEffect(() => {
+    if (choosedItems.length) {
+      setFieldValue("productId", choosedItems);
+    }
+  }, [choosedItems]);
+
+  useEffect(() => {
+    toast.error(errors.productId);
+  }, [errors]);
+
   return (
     <StyledCreateDiscount>
       <StyledModal open={openModal} handleClose={setOpenModal}>
         <Typography component="h1" variant="h5" className="flex center">
           Создать скидку
         </Typography>
-        <Box component="form" className="flex column gap2">
+        <Box
+          component="form"
+          className="flex column gap2"
+          onSubmit={handleSubmit}
+        >
           <Box className="forms">
             <FormLabel required>Размер скидки</FormLabel>
-            <StyledInput placeholder="0%" />
+            <StyledInput
+              placeholder="0%"
+              value={values.amountOfDiscount}
+              type="number"
+              onChange={handleChange}
+              name="amountOfDiscount"
+            />
+            {errors.amountOfDiscount && (
+              <Typography variant="subtitle2" color="error">
+                {errors.amountOfDiscount}
+              </Typography>
+            )}
+
             <StyledDatePicker
               date={dates}
               setDate={setDates}
               FormLabelTitle={["Дата начала скидки", "Дата окончания скидки"]}
+              errors={[errors.startDate || null, errors.endDate || null]}
             />
           </Box>
           <Box className="flex center gap2 buttons">
